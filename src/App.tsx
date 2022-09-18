9e3f776455eb468c4bed2a003fabb9690d23692e
Olga Strekalovskaia <afalina_91@mail.ru>
2022-09-18 15:41:51 +0300

    added variables for styles, organized switching of dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./index.scss"
+import "./styles/index.scss"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./styles/theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE BUTTON</button>
       <Link to='/about'>О сайте</Link>
       <Link to='/'>Главная</Link>
       <Suspense fallback={<div>Loading...</div>}>
