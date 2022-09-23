01d32f0fc7663719fe8ab3e304eeca6d64c202a5
Olga Strekalovskaia <afalina_91@mail.ru>
2022-09-23 17:17:53 +0300

    added ThemeSwitcher, custom component Button, svgLoader, fileLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,11 @@ import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -32,7 +37,18 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
+  const fileLoader =       {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
   return [
+    fileLoader,
+    svgLoader,
     typescriptLoader,
     cssLoader,
   ]
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ThemeSwitcher";
 import cls from "./Navbar.module.scss";
 
 
@@ -11,9 +12,14 @@ export const Navbar = ({ className }: NavbarProps) => {
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to='/about' className={cls.mainLink}>О сайте</AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to='/'>Главная</AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to='/about' className={cls.mainLink}>
+          О сайте
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to='/'>
+          Главная
+        </AppLink>
       </div>
     </div>
   )
