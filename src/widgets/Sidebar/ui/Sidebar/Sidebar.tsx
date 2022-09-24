1756e4b484b1d682bb11522ca31db8c9059290ed
Olga Strekalovskaia <afalina_91@mail.ru>
2022-09-24 11:13:08 +0300

    set up language switching (en, ru) using i18n, added LangSwitcher

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames'
+import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
 
@@ -21,6 +22,7 @@ export const Sidebar =({className}:SidebarProps) => {
       <button onClick={onToggle}>Toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher/>
+        <LangSwitcher className={cls.lang}/>
       </div>      
     </div>
   );
